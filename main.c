53314e17627d7357a72ba55129877cb43c67b72c
tgeorgymathew <tgeorgymathew@gmail.com>
2020-02-21 14:47:47 +0530

    Trial update

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 void main()
 {
     char *log = (char *) malloc(LOGSIZE);
-    strcpy(log , "INFO: Memory process started.\n");
+    strcpy(log , "INFO: Memory process started....\n");
     logger(log);
-    strcpy(log , "INFO: Memory process ended.\n");
+    strcpy(log , "INFO: Memory process ended...\n");
     logger(log);
 }

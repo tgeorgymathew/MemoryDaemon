2edf729f3425a7a0ec61849e5ebaebb867bf5ee5
tgeorgymathew <tgeorgymathew@gmail.com>
2020-02-21 14:59:07 +0530

    test-finl

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ void main()
     char *log = (char *) malloc(LOGSIZE);
     strcpy(log , "INFO: Memory process started....\n");
     logger(log);
-    strcpy(log , "INFO: Memory process ended...\n");
+    strcpy(log , "INFO: Memory process ended -1...\n");
+    logger(log);
+    strcpy(log , "INFO: Memory process ended -final...\n");
     logger(log);
 }
